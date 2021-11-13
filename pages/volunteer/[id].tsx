7edb00d65564a843399a305dd5c56a56fd0c843b
copninich <48505@yupparaj.ac.th>
2021-11-13 22:58:09 +0700

    update volunteer page

--- pages/volunteer/[id].tsx
+++ pages/volunteer/[id].tsx
@@ -13,6 +13,7 @@ export default function EditEntryPage() {
   const { data } = useVolunteering(id)
 
   if (data) {
+    if(data.v_ref != ''){
     return (
       <div>
         <Headset />
@@ -56,6 +57,50 @@ export default function EditEntryPage() {
         </body>
       </div>
     )
+    }else{
+      return (
+        <div>
+          <Headset />
+          <body className="index-page" style={{backgroundColor:"white"}}>
+              <Navbar/>
+              <div>
+                  <div className="py-sm-7 py-5 bg-banner min-vh-50">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12 mx-auto mt-4">
+                                <div className="row py-lg-4 py-5 mt-5 text-left">
+                                  <h6 className="text-white">กิจกรรมจิดอาสา</h6>
+                                  <h2 className="text-white">{data.v_name}</h2>
+                                  <h6 className="text-white"><i className="fas fa-calendar-week"></i>&nbsp;{data.v_year}</h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                  <div className="container">
+                      <div className="row">
+                        <div className="col-xl-12 mt-5">
+                            <div className="d-flex justify-content-center">
+                              <img src={data.v_pic} className="img-fluid w-xl-50" />
+                            </div>
+             
+                            <p>{data.v_detail}</p>   
+                           
+                        </div>
+                          
+                          
+                          
+                      </div>
+                      
+                  </div>
+                         
+              </div>
+              <Footer />
+              <Corejs />
+          </body>
+        </div>
+      )
+    }
   } else {
     return (
     <body className="index-page">
